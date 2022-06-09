f48623215ce3cba2e9115c6685753ec5e03cebed
Quoc-Hao Tran <quoc-hao.tran@polymtl.ca>
2022-06-09 12:02:24 -0400

    Keep all traces
    Signed-off-by: Quoc-Hao Tran <quoc-hao.tran@polymtl.ca>

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,8 +1,24 @@
 import { TsTrace } from "ts-trace-event";
 import fs from "fs";
+import path from "path";
+
+const ensureDirExist = (file_path: string) => {
+    const dirname = path.dirname(file_path);
+    if (!fs.existsSync(dirname)) {
+        ensureDirExist(dirname);
+        fs.mkdirSync(dirname);
+    }
+    return file_path;
+};
+
+const basename = `trace-event-formats/trace`;
+let i = 0;
+while (fs.existsSync(`${basename}-${i}.json`)) {
+    i++;
+}
 
 export const tracer = new TsTrace();
-tracer.pipe(fs.createWriteStream(`trace-event-format.json`, `utf8`));
+tracer.pipe(fs.createWriteStream(ensureDirExist(`${basename}-${i}.json`), `utf8`));
 process.on(`SIGINT`, () => process.exit(0));
 process.on(`exit`, (code) => {
     tracer.close();
